cca0d33260a45f267866503aac0bef9dfccf39f3
JasonKim0122 <JasonKim0122@gmail.com>
2022-07-03 14:53:52 -0500

    created thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    removeThought,
+    addReactions,
+    removeReactions
+} = require('../../controllers/thought-controller');
+
+router
+.route('/')
+.get(getAllThoughts)
+.post(createThought);
+
+router
+.route('/')
+.get(getThoughtById)
+.put(updateThought)
+.delete(removeThought)
+
+router
+.route('/:thoughtId/reactions')
+.post(addReactions);
+
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(removeReactions);
+
+module.exports = router;
